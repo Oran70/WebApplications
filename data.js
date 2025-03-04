af0a77e8349c9b1d3c65d6860963b3e80cc98c52
Oran70 <19140134@student.hss.nl>
2025-03-04 21:03:22 +0100

    Initial commit - added website files

---
+++ data.js
@@ -0,0 +1,107 @@
+const artworks = [
+    {
+        name: "Ethereal Veins",
+        fruitType: "Grape-Fruit",
+        group: "G1",
+        plantedDate: "2022-01-10",
+        artworkDate: "2023-06-15",
+        image: "images/GrapeFruit-001-G1.PNG"
+    },
+    {
+        name: "Whispers of the Earth",
+        fruitType: "Grape-Fruit",
+        group: "G1",
+        plantedDate: "2022-02-05",
+        artworkDate: "2023-07-10",
+        image: "images/GrapeFruit-002-G1.PNG"
+    },
+    {
+        name: "Silent Symphony",
+        fruitType: "Lemon",
+        group: "G3",
+        plantedDate: "2021-12-01",
+        artworkDate: "2023-04-20",
+        image: "images/Lemon-004-G3.PNG"
+    },
+    {
+        name: "Rooted Elegance",
+        fruitType: "Lemon",
+        group: "G4",
+        plantedDate: "2022-03-15",
+        artworkDate: "2023-08-01",
+        image: "images/Lemon-005-G4.PNG"
+    },
+    {
+        name: "Nature’s Embrace",
+        fruitType: "Lemon",
+        group: "G2",
+        plantedDate: "2021-11-10",
+        artworkDate: "2023-05-05",
+        image: "images/Lemon-006-G2.PNG"
+    },
+    {
+        name: "Echoes of Time",
+        fruitType: "Lemon",
+        group: "G2",
+        plantedDate: "2022-04-08",
+        artworkDate: "2023-09-25",
+        image: "images/Lemon-007-G2.PNG"
+    },
+    {
+        name: "Fossilized Whimsy",
+        fruitType: "Lemon",
+        group: "G3",
+        plantedDate: "2022-06-12",
+        artworkDate: "2023-10-12",
+        image: "images/Lemon-008-G3.PNG"
+    },
+    {
+        name: "Fractured Serenity",
+        fruitType: "Lemon",
+        group: null,
+        plantedDate: "2022-05-17",
+        artworkDate: "2023-11-01",
+        image: "images/Lemon-009.PNG"
+    },
+    {
+        name: "Golden Thread of Life",
+        fruitType: "Lemon",
+        group: "G4",
+        plantedDate: "2021-10-22",
+        artworkDate: "2023-12-05",
+        image: "images/Lemon-010-G4.PNG"
+    },
+    {
+        name: "Tangled Memories",
+        fruitType: "Passion-Fruit",
+        group: "G1",
+        plantedDate: "2022-09-14",
+        artworkDate: "2023-06-30",
+        image: "images/PassionFruit-003-G1.PNG"
+    },
+    {
+        name: "The Silent Bloom",
+        fruitType: "Lemon",
+        group: null,
+        plantedDate: "2022-09-14",
+        artworkDate: "2023-06-30",
+        image: "images/Lemon-011.PNG"
+    },
+    {
+        name: "Windswept Essence",
+        fruitType: "Lemon",
+        group: null,
+        plantedDate: "2022-09-14",
+        artworkDate: "2023-06-30",
+        image: "images/Lemon-012.PNG"
+    },
+    {
+        name: "Eclipsed Growth",
+        fruitType: "Lemon",
+        group: null,
+        plantedDate: "2022-09-14",
+        artworkDate: "2023-06-30",
+        image: "images/Lemon-013.PNG"
+    }
+
+];
